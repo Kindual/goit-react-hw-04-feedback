a433d2535c828cc41db20815b746d3f51ea309ec
Kindual <110603179+Kindual@users.noreply.github.com>
2023-03-24 00:10:34 +0200

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Notification from './Notification/Notification';
 import Section from './Section/Section';
